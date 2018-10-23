209de853a303f7b348fab186c1f38c3c49fe45e8
long đoàn <doanthanhlong1801@gmail.com>
2018-10-23 11:13:39 +0700

    push code

---
+++ CS447/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CS447.MvcApplication" Language="C#" %>
---
+++ CS447/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CS447.Startup))]
+namespace CS447
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
